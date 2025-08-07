ce5800576d5cc9f3c58c1cecafec6804858992ce
brenopinheiro-programador <pinheirobreno013@gmail.com>
2025-08-07 04:07:12 +0000

    antecessor e sucessor de um número escolhido

--- desafio1/calculo.php
+++ desafio1/calculo.php
@@ -12,7 +12,7 @@
         <p>
             <?php
                 $num = $_GET["number"] ?? 0; //pegando o valor do input
-                echo "O número escolhido foi <strong> $num </strong>"; //exibindo na tela o Nº escolhido
+                echo "O número escolhido foi <strong> $num </strong>"; //exibindo na tela o Nº escolhido.
                 echo "<br>O seu <em>antecessor</em> é ". $num - 1 .""; //exibindo seu antecessor
                 echo "<br>O seu <em>sucessor</em> é ". $num + 1 .""; //exibindo seu sucessor
             ?>
